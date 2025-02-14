ee3be91ceae3c36091ef22456b8365df918e2155
Peter <garciajeter9@gmail.com~>
2025-02-14 18:12:16 -0400

    Actualizacion Clave para blog

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react";
 import Image from "next/image";
-import { motion } from "framer-motion";
 import { format } from "date-fns";
 import { es } from "date-fns/locale";
 import Link from "next/link";
