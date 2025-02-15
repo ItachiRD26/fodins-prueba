fc755f95586f8cbcef74ff18391af76728988d91
Peter <garciajeter9@gmail.com~>
2025-02-15 02:32:20 -0400

    Actualizaciones casi Finales

--- src/app/ministerio/firebaseConfig.js
+++ src/app/ministerio/firebaseConfig.js
@@ -11,6 +11,7 @@ const firebaseConfig = {
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   databaseURL: process.env.NEXT_PUBLIC_REALTIME_DATABASE_URL,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Inicializa Firebase
