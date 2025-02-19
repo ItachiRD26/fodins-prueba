d5667ba21f36fbd0a9bd1dbe4c5179cd48953166
Peter <garciajeter9@gmail.com~>
2025-02-19 14:30:59 -0400

    Problemas con Servidor

--- src/app/ministerio/login/api/verifyuser/route.js
+++ src/app/ministerio/login/api/verifyuser/route.js
@@ -1,5 +1,5 @@
-import { verifyUser } from "../../../firebase/firebaseAdmin"
-import { NextResponse } from "next/server"
+import { verifyUser } from "../../../firebase/firebaseAdmin";
+import { NextResponse } from "next/server";
 
 export async function POST(req) {
   // Configurar los headers CORS
@@ -7,24 +7,27 @@ export async function POST(req) {
     "Access-Control-Allow-Origin": "https://fodins-website.vercel.app",
     "Access-Control-Allow-Methods": "POST, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
-  })
+  });
 
   // Manejar la solicitud OPTIONS para CORS preflight
   if (req.method === "OPTIONS") {
-    return new NextResponse(null, { status: 204, headers })
+    return new NextResponse(null, { status: 204, headers });
   }
 
   try {
-    const { uid } = await req.json()
+    const { uid } = await req.json();
+    console.log("UID recibido:", uid);
 
-    if (await verifyUser(uid)) {
-      return NextResponse.json({ message: "Usuario verificado" }, { status: 200, headers })
+    const isVerified = await verifyUser(uid);
+    console.log("Resultado de verifyUser:", isVerified);
+
+    if (isVerified) {
+      return NextResponse.json({ message: "Usuario verificado" }, { status: 200, headers });
     } else {
-      return NextResponse.json({ message: "Acceso denegado" }, { status: 403, headers })
+      return NextResponse.json({ message: "Acceso denegado" }, { status: 403, headers });
     }
   } catch (error) {
-    console.error("Error en verifyToken:", error)
-    return NextResponse.json({ message: "Error interno del servidor" }, { status: 500, headers })
+    console.error("Error en verifyToken:", error);
+    return NextResponse.json({ message: "Error interno del servidor" }, { status: 500, headers });
   }
 }
-
